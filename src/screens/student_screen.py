29042819c056cc0ff9b830ac9c73ff00f0f0b4a7
Sakshi-2565 <your-email@example.com>
2026-09-16 12:21:52 +0530

    Fix student welcome f-string

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -19,7 +19,7 @@ def student_dashboard():
     with col1:
         header_dashboard()
     with col2:
-        st.subheader(f"Welcome {student_data["name"]}")
+        st.subheader(f"Welcome {student_data['name']}")
         if st.button("Logout",type='primary',width='content',key='loginbackbtn',shortcut="control+backspace"):
             st.session_state["is_logged_in"]=False
             del st.session_state.student_data
